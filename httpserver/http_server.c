b316861aab0fd3a41516e76880508e40d7423a9b
Jonah Turrell <jonahturrell@gmail.com>
2023-02-13 15:32:13 -0500

    Complete refactor

--- httpserver/http_server.c
+++ httpserver/http_server.c
@@ -1,84 +1,130 @@
+#include "http_server.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <errno.h>
-#include <stdbool.h>
 
-#include <sys/types.h>
-#include <sys/socket.h>
-
-#include <netinet/in.h>
-
-void err_n_die(char *message, bool die);
+#define PORT "8080"
+#define MAX_QUEUED 10
+#define MAX_REQUEST_SIZE 1024
 
 int main(void) {
-  
-  // Load a pointer to the index.html file
-  FILE *html_data;
-  html_data = fopen("index.html", "r");
 
-  if (html_data == NULL)   //Make sure index.html is a proper file
-    err_n_die("Issue opening index.html", true);
-
-  // Create a string and store index.html contents into it
-  char response_data[1024];
-  fgets(response_data, 1024, html_data);
-
-  // Create the HTTP response
-  char http_header[2048] = "HTTP/1.1 200 OK\r\n\n";
-
-  // Concatenate the HTTP response and response data
-  strcat(http_header, response_data);
-
-  // Create a socket
-  int server_socket;
-  server_socket = socket(AF_INET, SOCK_STREAM, 0);
-
-  if (server_socket <= 0) // Make sure the socket is created properly
-    err_n_die("Creating the socket failed", true);
-  
-  // Define the address
-  struct sockaddr_in server_address;
-  server_address.sin_family = AF_INET;
-  server_address.sin_port = htons(8002);
-  server_address.sin_addr.s_addr = INADDR_ANY;
-
-  // Bind the socket to the port
-  if (bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address)) != 0) // Make sure the socket binds to the port properly
-    err_n_die("Issue binding the socket to the port", true);
-
-  //Listen for connections
-  if (listen(server_socket, 5) != 0) // Make sure the socket listens properly
-    err_n_die("Issue listening on socket", true);
-
-  //Make a variable for the client socket
-  int client_socket;
-
-  //Continually accept connections and send the HTTP data
-  while (1) {
-    client_socket = accept(server_socket, NULL, NULL);
-    if (client_socket < 0) {
-      err_n_die("Issue accepting connection", false);
-      printf("Moving to next connection...");
-      close(client_socket);
-      continue;
+  //  Initialize Winsock if on Windows
+  #if defined(_WIN32)
+    WSADATA d;
+    if (WSAStartup(MAKEWORD(2, 2), &d)) {
+      fprintf(stderr, "Winsock failed to initialize.\n");
+      return 1;
     }
+  #endif
 
-    printf("Got connection\n");
-    send(client_socket, http_header, sizeof(http_header), 0);
-    close(client_socket);
+  //  Configure the local address
+  printf("Configuring local address...\n");
+  struct addrinfo hints;
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
+
+  // Store the local address in a structure
+  struct addrinfo *bind_address;
+  int addrinfo_return = getaddrinfo(0, PORT, &hints, &bind_address);
+
+  if (addrinfo_return != 0) { // Make sure a valid address is given
+    fprintf(stderr, "getaddrinfo() failed; %s\n", gai_strerror(addrinfo_return));
+    return EXIT_FAILURE;
   }
-  
-  return 0;
-}
 
-void err_n_die(char *message, bool die) {
-  printf("ERROR: %s\n", message);
-  printf("Code %d: %s\n", errno, strerror(errno));
+  //  Create the socket
+  printf("Creating socket...\n");
+  SOCKET socket_listen; 
+  socket_listen = socket(bind_address->ai_family, bind_address->ai_socktype, bind_address->ai_protocol);
 
-  if (die == true) {
-    printf("Exiting...\n");
-    exit(EXIT_FAILURE);
+  if (!ISVALIDSOCKET(socket_listen)) { // Check that the socket was successfully created
+    fprintf(stderr, "socket() failed. (%d)\n", GETSOCKETERRNO());
+    return EXIT_FAILURE;
   }
+
+  //  Bind socket to the address
+  printf("Binding socket to local address...\n");
+  if (bind(socket_listen, bind_address->ai_addr, bind_address->ai_addrlen)) { //  Make sure the socket is bound properly
+    fprintf(stderr, "bind() failed. (%d)\n", GETSOCKETERRNO());
+    return EXIT_FAILURE;
+  }
+  //  Free the bind_address memory
+  freeaddrinfo(bind_address);
+
+  //  Listen for a connection
+  printf("Listening for connections...\n");
+  if (listen(socket_listen, MAX_QUEUED) < 0) { // Make sure listen() works properly
+    fprintf(stderr, "listen() failed. (%d)\n", GETSOCKETERRNO());
+    return EXIT_FAILURE;
+  }
+
+  // Wait for a connection
+  printf("Waiting for a connection...\n");
+
+  struct sockaddr_storage client_address;  // Set up stucture for client address information
+  socklen_t client_len = sizeof(client_address);
+
+  SOCKET socket_client = accept(socket_listen, (struct sockaddr*) &client_address, &client_len);
+
+  if (!ISVALIDSOCKET(socket_client)) {
+  fprintf(stderr, "accept() failed. (%d)\n", GETSOCKETERRNO());
+  return EXIT_FAILURE;
+  }
+
+  printf("Client is connected...\n");
+  char address_buffer[512];
+  int getnameinfo_return = getnameinfo((struct sockaddr*) &client_address, client_len, address_buffer, sizeof(address_buffer), 0, 0, NI_NUMERICHOST);
+
+  if (getnameinfo_return != 0) { // Check that the client address information is properly returned
+    printf("getnameinfo() failed. %s\n", gai_strerror(getnameinfo_return));
+  }
+
+  printf("Client IP: %s\n", address_buffer);
+
+  //  Attempt to read the client request
+  printf("Reading client request...\n");
+  char request[MAX_REQUEST_SIZE];
+  int bytes_received = recv(socket_client, request, MAX_REQUEST_SIZE, 0);
+
+  if (bytes_received < 0) { //  Check that bytes are received properly
+    printf("recv() failed. (%d)\n", GETSOCKETERRNO());
+  } else {
+    printf("Received %d bytes\n", bytes_received);
+    printf("Request contents:\n%.*s\n", bytes_received, request);
+  }
+
+  //  Attempt to send a response
+  printf("Sending response...\n");
+
+  const char *response =
+    "HTTP/1.1 CONNECTION OK\r\n"
+    "Connection: close\r\n"
+    "Content-Type: text/plain\r\n\r\n"
+    "<html><body>This is a webpage</body></html>";
+
+  int bytes_sent = send(socket_client, response, strlen(response), 0);
+  if (bytes_sent < 0) {
+    printf("send() failed. (%d)\n", GETSOCKETERRNO());
+  } else {
+    printf("Sent %d of %lu bytes\n", bytes_sent, strlen(response));
+  }
+
+  // Close the connection
+  printf("Closing connection...\n");
+  CLOSESOCKET(socket_client);
+
+  printf("Closing listening socket...\n");
+  CLOSESOCKET(socket_listen);
+
+  #if defined(_WIN32)
+   WSACleanup();
+  #endif
+
+  printf("Finished\n");
+ 
+  return EXIT_SUCCESS;
 }
